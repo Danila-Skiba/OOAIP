4ca8fdf81559bb8946aa6b8b7624184c6cf3e565
Skripa Andrey <stefan.richard.sk@gmail.com>
2024-11-06 17:23:12 +0600

    Class1.cs

--- SpaceBattle/SpaceBattle.Lib/Class1.cs
+++ SpaceBattle/SpaceBattle.Lib/Class1.cs
@@ -1,6 +1,74 @@
 ﻿namespace SpaceBattle.Lib;
-
-public class Class1
 {
 
+    public struct Vector
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+
+        // Конструктор
+        public Vector(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+    
+    public interface IMovable
+    {
+        Vector Position { get; set; }
+        Vector Velocity { get; }
+
+        void StartMove(Vector velocity);
+        void StopMove();
+    }
+
+    public interface IRotatable
+    {
+        double Angle { get; set; }
+
+        double AngularVelocity { get; }
+
+        void StartRotate(double angularVelocity);
+
+        void StopRotate();
+    }
+
+    namespace SpaceBattle.Lib
+{
+    public class MoveCommand : ICommand
+    {
+        private IMovable _movable;
+
+        public MoveCommand(IMovable movable)
+        {
+            _movable = movable;
+        }
+
+        public void Execute()
+        {
+            //логики
+        }
+    }
+
+        public class RotateCommand : ICommand
+    {
+        private IRotatable _rotatable;
+
+        public RotateCommand(IRotatable rotatable)
+        {
+            _rotatable = rotatable;
+        }
+
+        public void Execute()
+        {
+            //логика
+        }
+    }
+
+
 }
+
+
+}
+
