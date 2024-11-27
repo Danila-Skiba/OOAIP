794c369fe62ecf2b08d8514a518205152912d858
Danila-Skiba <144258867+Danila-Skiba@users.noreply.github.com>
2024-11-27 21:58:05 +0600

    Update UnitTest1.cs

--- SpaceBattle/SpaceBattle.Lib.Tests/UnitTest1.cs
+++ SpaceBattle/SpaceBattle.Lib.Tests/UnitTest1.cs
@@ -1,10 +1,23 @@
-namespace SpaceBattle.Lib.Tests;
+using Xunit;
 
-public class UnitTest1
+namespace SpaceBattle.Lib.Tests
 {
-    [Fact]
-    public void Test1()
+    public class SomeClassTests
     {
-         Assert.True(true);
+        [Fact]
+        public void CalculateDamage_PositiveDamage()
+        {
+            var someClass = new SomeClass();
+            var damage = someClass.CalculateDamage(10, 5);
+            Assert.Equal(5, damage);
+        }
+
+        [Fact]
+        public void CalculateDamage_ZeroDamage()
+        {
+            var someClass = new SomeClass();
+            var damage = someClass.CalculateDamage(5, 10);
+            Assert.Equal(0, damage);
+        }
     }
 }
