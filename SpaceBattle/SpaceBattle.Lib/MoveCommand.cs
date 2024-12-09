788de1ba5ffbe18836050432bdfec5c2a926c28e
Smolovaia Zhanna <zs_e7@mail.ru>
2024-12-09 14:48:20 +0600

    update to new requirements

---
+++ SpaceBattle/SpaceBattle.Lib/MoveCommand.cs
@@ -0,0 +1,16 @@
+using SpaceBattle.Lib;
+namespace SpaceBattle.Lib
+{
+    public class MoveCommand : ICommand
+    {
+        private readonly IMoving moving;
+        public MoveCommand(IMoving moving)
+        {
+            this.moving = moving;
+        }
+        public void Execute()
+        {
+            moving.Position += moving.Velocity;
+        }
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Lib/Vector.cs
@@ -0,0 +1,61 @@
+namespace SpaceBattle.Lib;
+using SpaceBattle.Lib;
+public class Vector
+{
+    private readonly int[] coordinates;
+
+    public Vector(int[] coord)
+    {
+        coordinates = coord;
+    }
+
+    public int[] GetCoordinates()
+    {
+        return (int[])coordinates.Clone();
+    }
+
+    public override bool Equals(object? obj)
+    {
+
+        if (obj is Vector vector && vector.coordinates.Length == this.coordinates.Length) 
+        {
+            for (int i = 0; i < coordinates.Length; i++)
+            {
+                if (this.coordinates[i] != vector.coordinates[i]) return false;
+            }
+            return true;
+        }
+        return false;
+    }
+
+    public override int GetHashCode()
+    {
+        return System.HashCode.Combine(coordinates);
+    }
+
+    public static Vector operator +(Vector vector1, Vector vector2)
+    {
+        if (vector1.coordinates.Length != vector2.coordinates.Length)
+        {
+            throw new ArgumentException("Вектора должны иметь одинаковую размерность");
+        }
+
+        int[] result = new int[vector1.coordinates.Length];
+        for (int i = 0; i < result.Length; i++)
+        {
+            result[i] = vector1.coordinates[i] + vector2.coordinates[i];
+        }
+
+        return new Vector(result);
+    }
+
+    public static bool operator ==(Vector vector1, Vector vector2)
+    {
+        return vector1.Equals(vector2);
+    }
+
+    public static bool operator !=(Vector vector1, Vector vector2)
+    {
+        return !(vector1 == vector2);
+    }
+}
