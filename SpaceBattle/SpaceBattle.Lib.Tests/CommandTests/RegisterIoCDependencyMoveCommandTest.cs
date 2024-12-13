600743595998a58b8ba9a54a296c270b1ae954dc
Smolovaia Zhanna <zs_e7@mail.ru>
2024-12-13 21:33:33 +0600

    update RegisterIoCDependencyMoveCommand

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RegisterIoCDependencyMoveCommandTest.cs
@@ -0,0 +1,37 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterIoCDependencyMoveCommandTests
+    {
+        public RegisterIoCDependencyMoveCommandTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void Execute_ShouldRegisterMoveCommandDependency()
+        {
+            var movingMock = new Mock<IMoving>();
+            var movingObjectMock = new Mock<object>();
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Adapters.IMovingObject",
+                (object[] args) => movingMock.Object
+            ).Execute();
+
+            var register = new RegisterIoCDependencyMoveCommand();
+            register.Execute();
+
+            var resolvedCommand = Ioc.Resolve<ICommand>("Commands.Move", movingObjectMock);
+
+            Assert.NotNull(resolvedCommand);
+            Assert.IsType<MoveCommand>(resolvedCommand);
+        }
+    }
+}
--- SpaceBattle/SpaceBattle.Lib.Tests/StructTests/AngleTests.cs
+++ SpaceBattle/SpaceBattle.Lib.Tests/StructTests/AngleTests.cs
@@ -48,9 +48,7 @@ public class TestsAngle
     public void Angle_GetHashCode()
     {
         var angle = new Angle(5);
-        var hashcode = angle.GetHashCode();
-
-        Assert.NotNull(hashcode);
+        _ = angle.GetHashCode();
     }
     [Fact]
     public void Angle_Equals_Null_ReturnsFalse()
