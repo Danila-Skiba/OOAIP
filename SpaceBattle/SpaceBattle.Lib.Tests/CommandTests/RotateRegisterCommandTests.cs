b3e0409a143573d30c48615a110672211cf379f7
Danila-Skiba <fiks11pikvik@gmail.com>
2024-12-13 18:49:49 +0600

    Add Tests

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RotateRegisterCommandTests.cs
@@ -0,0 +1,40 @@
+﻿using App;
+using App.Scopes;
+using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class RegisterIoCDependencyRotateCommandTests
+    {
+
+        public RegisterIoCDependencyRotateCommandTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void Execute_ShouldRegisterRotateCommandDependencyTrue()
+        {
+            // Arrange
+            var rotatingMock = new Mock<IRotating>();
+            var mockGameObject = new Mock<object>();
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Adapters.IRotatingObject",
+                (object[] args) => rotatingMock.Object
+            ).Execute();
+
+            // Act
+            var register = new RegisterIoCDependencyRotateCommand();
+
+            register.Execute();
+
+            // Assert
+            var resolvedCommand = Ioc.Resolve<ICommand>("Commands.Rotate", mockGameObject.Object);
+            Assert.NotNull(resolvedCommand);
+            Assert.IsType<Rotate>(resolvedCommand);
+        }
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterRotateCommand.cs
@@ -0,0 +1,15 @@
+using SpaceBattle;
+using App;
+
+namespace SpaceBattle.Lib
+{
+    public class RegisterIoCDependencyRotateCommand : ICommand
+    {
+        public void Execute()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Commands.Rotate",
+                (object[] args) => new Rotate(Ioc.Resolve<IRotating>("Adapters.IRotatingObject", args[0]))).Execute();
+        }
+    }
+}
+
