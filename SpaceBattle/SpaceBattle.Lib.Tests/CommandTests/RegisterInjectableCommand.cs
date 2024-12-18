cb7102713c25f9ac895aa9e2394eb059e14f0ede
Danila-Skiba <fiks11pikvik@gmail.com>
2024-12-18 14:32:49 +0600

    format

--- SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RegisterInjectableCommand.cs
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/RegisterInjectableCommand.cs
@@ -2,13 +2,12 @@ using App;
 using App.Scopes;
 using Moq;
 
-namespace SpaceBattle.Tests
+namespace SpaceBattle.Lib.Tests
 {
     public class RegisterDependencyCommandInjectableCommandTests : IDisposable
     {
         public RegisterDependencyCommandInjectableCommandTests()
         {
-            // Инициализация IoC контейнера
             new InitCommand().Execute();
             var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
             Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
@@ -17,29 +16,19 @@ namespace SpaceBattle.Tests
         [Fact]
         public void RegisterDependencyCommandInjectableCommand_Should_Register_CommandInjectableCommand()
         {
-            // Создаем мок для ICommand
-            var commandMock = new Mock<ICommand>();
-            var command = commandMock.Object;
 
-            // Регистрируем ICommand в IoC
-            Ioc.Resolve<App.ICommand>(
-                "IoC.Register",
-                "Commands.CommandInjectable",
-                (object[] args) => new CommandInjectableCommand()
-            ).Execute();
-
-            // Создаем и выполняем команду регистрации
             var registerCommand = new RegisterDependencyCommandInjectableCommand();
             registerCommand.Execute();
 
-            // Проверяем, что зависимости разрешаются без исключений
-            var commandInjectable = Ioc.Resolve<ICommandInjectable>("Commands.CommandInjectable");
+
+            var command = Ioc.Resolve<ICommand>("Commands.CommandInjectable");
+            var injectableCommand = Ioc.Resolve<ICommandInjectable>("Commands.CommandInjectable");
             var commandInjectableCommand = Ioc.Resolve<CommandInjectableCommand>("Commands.CommandInjectable");
 
-            Assert.NotNull(commandInjectable);
+            Assert.NotNull(command);
+            Assert.NotNull(injectableCommand);
             Assert.NotNull(commandInjectableCommand);
         }
-
         public void Dispose()
         {
             Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
