6670e2291a923710435c576c9a43424078d6dbba
Smolovaia Zhanna <zs_e7@mail.ru>
2025-02-25 21:08:04 +0600

    feat: add FireCommand

---
+++ SpaceBattle/SpaceBattle.Lib/Commands/RegisterFireDependencies.cs
@@ -0,0 +1,14 @@
+using App;
+namespace SpaceBattle.Lib
+{
+    public class RegisterFireDependencies: ICommand
+    {
+        public boid Execute()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Register", "Commands.Fire", (object[] args) =>
+            new FireCommand(Ioc.Resolve<IFireable>("Adapters.IFireableObject", args[0]),
+            Ioc.Resolve<IWeaponFactory>("WeaponFactory"),
+            Ioc.Resolve<IGameObjectRepository>("Game.Objects"))).Execute();
+        }
+    }
+}
