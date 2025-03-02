4a8d7dd19dc1a432bf5056b286729ae5430b06e1
tatsivo <t-pudovkina@inbox.ru>
2025-03-02 19:00:26 +0700

    add AuthCommand

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/AuthCommandTests.cs
@@ -0,0 +1,127 @@
+using App;
+using Moq;
+using System;
+using System.Collections.Generic;
+using Xunit;
+using App.Scopes;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class AuthCommandTests : IDisposable
+    {
+         public AuthCommandTests()
+        {
+            new InitCommand().Execute();
+            var iocScope = Ioc.Resolve<object>("IoC.Scope.Create");
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Set", iocScope).Execute();
+        }
+
+        [Fact]
+        public void Execute_PlayerOwnsObjectAndHasPermission_Success()
+        {
+            // Arrange
+            var playerId = "player1";
+            var objectId = "ship1";
+            var operation = "Fire";
+
+            var playerObjects = new List<string> { objectId };
+            var playerPermissions = new Dictionary<string, List<string>>
+            {
+                { objectId, new List<string> { operation } }
+            };
+
+            // Регистрируем зависимости в IoC
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Players.GetObjects",
+                (object[] args) => playerObjects
+            ).Execute();
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Players.GetPermissions",
+                (object[] args) => playerPermissions
+            ).Execute();
+
+            var authCommand = new AuthCommand(playerId, objectId, operation);
+
+            // Act
+            var exception = Record.Exception(() => authCommand.Execute());
+
+            // Assert
+            Assert.Null(exception); // Ожидаем, что исключение не будет выброшено
+        }
+
+        [Fact]
+        public void Execute_PlayerDoesNotOwnObject_ThrowsUnauthorizedAccessException()
+        {
+            // Arrange
+            var playerId = "player1";
+            var objectId = "ship1";
+            var operation = "Fire";
+
+            var playerObjects = new List<string> { "ship2" }; // Игрок не владеет ship1
+            var playerPermissions = new Dictionary<string, List<string>>
+            {
+                { objectId, new List<string> { operation } }
+            };
+
+            // Регистрируем зависимости в IoC
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Players.GetObjects",
+                (object[] args) => playerObjects
+            ).Execute();
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Players.GetPermissions",
+                (object[] args) => playerPermissions
+            ).Execute();
+
+            var authCommand = new AuthCommand(playerId, objectId, operation);
+
+            // Act & Assert
+            var exception = Assert.Throws<UnauthorizedAccessException>(() => authCommand.Execute());
+            Assert.Equal($"Player {playerId} does not own object {objectId}.", exception.Message);
+        }
+
+        [Fact]
+        public void Execute_PlayerOwnsObjectButNoPermission_ThrowsUnauthorizedAccessException()
+        {
+            // Arrange
+            var playerId = "player1";
+            var objectId = "ship1";
+            var operation = "Fire";
+
+            var playerObjects = new List<string> { objectId };
+            var playerPermissions = new Dictionary<string, List<string>>
+            {
+                { objectId, new List<string> { "Move" } } // Нет права на "Fire"
+            };
+
+            // Регистрируем зависимости в IoC
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Players.GetObjects",
+                (object[] args) => playerObjects
+            ).Execute();
+
+            Ioc.Resolve<App.ICommand>(
+                "IoC.Register",
+                "Players.GetPermissions",
+                (object[] args) => playerPermissions
+            ).Execute();
+
+            var authCommand = new AuthCommand(playerId, objectId, operation);
+
+            // Act & Assert
+            var exception = Assert.Throws<UnauthorizedAccessException>(() => authCommand.Execute());
+            Assert.Equal($"Player {playerId} is not authorized to perform operation '{operation}' on object {objectId}.", exception.Message);
+        }
+        public void Dispose()
+        {
+            Ioc.Resolve<App.ICommand>("IoC.Scope.Current.Clear").Execute();
+        }
+    }
+}
---
+++ SpaceBattle/SpaceBattle.Lib/Commands/AuthCommand.cs
@@ -0,0 +1,40 @@
+using App;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SpaceBattle.Lib
+{
+    public class AuthCommand : ICommand
+    {
+        private readonly string _playerId;
+        private readonly string _objectId;
+        private readonly string _operation;
+
+        public AuthCommand(string playerId, string objectId, string operation)
+        {
+            _playerId = playerId;
+            _objectId = objectId;
+            _operation = operation;
+        }
+
+        public void Execute()
+        {
+            var playerObjects = Ioc.Resolve<IEnumerable<string>>("Players.GetObjects", _playerId);
+
+            if (!playerObjects.Contains(_objectId))
+                throw new UnauthorizedAccessException($"Player {_playerId} does not own object {_objectId}.");
+
+            // Получаем права (разрешенные операции) для этого объекта
+            var playerPermissions = Ioc.Resolve<Dictionary<string, List<string>>>("Players.GetPermissions", _playerId);
+
+            // Проверяем, что у игрока есть право на выполнение операции над этим объектом
+            if (!playerPermissions.TryGetValue(_objectId, out var permissions) || !permissions.Contains(_operation))
+            {
+                throw new UnauthorizedAccessException($"Player {_playerId} is not authorized to perform operation '{_operation}' on object {_objectId}.");
+            }
+
+            // Если проверки прошли успешно, авторизация завершена
+        }
+    }
+}
