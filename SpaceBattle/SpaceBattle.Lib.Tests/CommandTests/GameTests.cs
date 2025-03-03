073b3374949c96ea29e0fea833eb26a02f247076
no7yao <dad62482@gmail.com>
2025-03-03 10:11:33 +0300

    gameclass

---
+++ SpaceBattle/SpaceBattle.Lib.Tests/CommandTests/GameTests.cs
@@ -0,0 +1,63 @@
+﻿using Moq;
+
+namespace SpaceBattle.Lib.Tests
+{
+    public class GameTests
+    {
+
+        [Fact]
+        public void ExecuteAllCommands()
+        {
+            var game = new Game();
+
+            var cmd1 = new Mock<ICommand>();
+            var cmd2 = new Mock<ICommand>();
+
+            game.Receive(cmd1.Object);
+            game.Receive(cmd2.Object);
+
+            game.Execute();
+
+            cmd1.Verify(c => c.Execute(), Times.Once);
+            cmd2.Verify(c => c.Execute(), Times.Once);
+        }
+
+        [Fact]
+        public void ExecuteQueueIsEmpty()
+        {
+            var game = new Game();
+
+            game.Execute();
+
+            Assert.True(true);
+        }
+
+        [Fact]
+        public void ExecuteCommandsException()
+        {
+            var game = new Game();
+
+            var cmd1 = new Mock<ICommand>();
+            var cmd2 = new Mock<ICommand>();
+
+            cmd1.Setup(c => c.Execute()).Throws(new Exception());
+
+            game.Receive(cmd1.Object);
+            game.Receive(cmd2.Object);
+
+            game.Execute();
+
+            cmd1.Verify(c => c.Execute(), Times.Once);
+            cmd2.Verify(c => c.Execute(), Times.Once);
+        }
+
+        [Fact]
+        public void ExceptionCommandNull()
+        {
+            var game = new Game();
+
+            var exception = Assert.Throws<ArgumentNullException>(() => game.Receive(null));
+            Assert.Equal("cmd", exception.ParamName);
+        }
+    }
+}
